68c709e998a0855f4bba8ecc25c6c991a1a8fe52
Molybdeen-42 <bmkiezebrink@proton.me>
2026-02-25 21:01:09 +0100

    Added changes to included font version, added oh-my-zsh and configured the proper locations for extensions. Additionally added a backup for the .zshrc file on the system.

--- auto-term-pimp.sh
+++ auto-term-pimp.sh
@@ -2,17 +2,19 @@
 
 echo "Installing required font..."
 
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/FiraMonoNerdFont-Regular.otf -P ~/.local/share/fonts/
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/FiraMonoNerdFontMono-Regular.otf -P "$HOME/.local/share/fonts/"
 
 echo "Downloading themes from GitHub..."
 
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting"
+git clone https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/themes/powerlevel10k"
 
-echo "Configuring zshell..."
+echo "Configuring .zshrc (zshell)..."
 
-cat zshrc.txt > ~/.zshrc
-chsh -s /bin/zsh
+mv "$HOME/.zshrc" "$HOME/.zshrc-old"
+cp zshrc.txt  "$HOME/.zshrc"
+chsh -s "$(which zsh)"
 
-echo "Please restart your machine and enjoy your new zshell theme! :)"
+echo "Please restart your terminal/machine and enjoy your new zshell theme! :)"
